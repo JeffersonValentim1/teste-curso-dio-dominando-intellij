620485c79dae28a04afea31c05510723a98d5de4
Your Name <jeffersonvalentim2014@gmail.com>
2022-02-10 00:05:19 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gatto = new Gato();
+        System.out.println(gatto);
+    }
+}
